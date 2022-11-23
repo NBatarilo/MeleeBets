b8b4000098584da8b57bdc22314923e2507f7495
NBatarilo <batarilo.nick841@gmail.com>
2022-11-23 16:13:34 -0500

    lol

---
+++ frontend/src/app/navabr/navbar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    standalone: true,
+    selector: 'navbar',
+    imports: [
+    ],
+    templateUrl: './navbar.component.html'
+  })
+
+  export class NavbarComponent {}
