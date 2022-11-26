35d48f4ad7e4f6ff3ac16ac7365de551817bfa45
Tim <vantimothy10@gmail.com>
2022-11-25 22:37:56 -0500

    fix routing and common module import

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { RouterModule } from '@angular/router';
-import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
-import { BetsComponent } from './bets/bets.component';
 import { BetsModule } from './bets/bets.module';
 import { UsersModule } from './users/users.module';
 
